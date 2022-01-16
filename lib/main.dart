b3da68dbafdd8b0920dc242f1145d94af99111fc
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-01-16 12:33:58 -0500

    configurando navbar theme

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,12 @@ class MyApp extends StatelessWidget {
         'home': (_) => HomePage(),
         'mapa': (_) => MapaPage(),
       },
+      theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+          floatingActionButtonTheme:
+              FloatingActionButtonThemeData(backgroundColor: Colors.deepPurple),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              selectedItemColor: Colors.deepPurple)),
     );
   }
 }
