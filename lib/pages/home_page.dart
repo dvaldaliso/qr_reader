c99f32f57829bdc6396e6a8067b021fcad57b1b9
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-01-16 12:24:35 -0500

    diseno del boton flotante y el navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(onPressed: null, icon: Icon(Icons.delete_forever))
+        ],
+      ),
       body: Center(
         child: Text('Home Page'),
       ),
+      bottomNavigationBar: CustomNavigatorBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
