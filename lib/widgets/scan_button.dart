87c54d00ffa9970259f1a84584dc3684ae8d3c10
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-01-20 23:52:40 -0500

    scanner

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
@@ -6,6 +8,14 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-        elevation: 0, child: Icon(Icons.filter_center_focus), onPressed: () {});
+        elevation: 0,
+        child: Icon(Icons.filter_center_focus),
+        onPressed: () async {
+          String barcodeScanRes;
+          // Platform messages may fail, so we use a try/catch PlatformException.
+          barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+              '#ff6666', 'Cancelar', true, ScanMode.QR);
+          print(barcodeScanRes);
+        });
   }
 }
